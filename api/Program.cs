f00855b6579e2cb651952ca1fb401af066717667
Ronaldo da Silva <ronaldo.intermec@gmail.com>
2025-01-01 17:30:05 -0300

    added Microsoft.EntityFrameworkCore

--- api/Program.cs
+++ api/Program.cs
@@ -1,80 +1,123 @@
 using Microsoft.AspNetCore.Authentication.OAuth.Claims;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
-app.MapGet("/user",  () => new { Name = "Ronaldo Silva", Age = 44});
+app.MapGet("/user", () => new { Name = "Ronaldo Silva", Age = 44 });
 
 
-app.MapGet("/addheader", (HttpResponse response) => {
+app.MapGet("/addheader", (HttpResponse response) =>
+{
     response.Headers["Teste"] = "Ronaldo Silva"; // This will replace the value if it already exists
     return new { Name = "Ronaldo Silva", Age = 44 };
 });
 
-app.MapPost("/product",(Product product) =>{
+app.MapPost("/product", (Product product) =>
+{
 
     ProductRepository.Add(product);
     return Results.Created("/product" + product.Code, product.Code);
 });
 
 // http://localhost:3000/getproduct?dateStart=2024-10-01&dateEnd=2024-11-22
-app.MapGet("/product", ([FromQuery] string dateStart, [FromQuery] string dateEnd) =>{
+app.MapGet("/product", ([FromQuery] string dateStart, [FromQuery] string dateEnd) =>
+{
     return dateStart + " - " + dateEnd;
 });
 
 // http://localhost:3000/getproduct/123456780?
-app.MapGet("/product/{code}", ( [FromRoute] string code) => {
-    return ProductRepository.GetBy(code);
-});
+app.MapGet("/product/{code}", ([FromRoute] string code) =>
 
+{
+    var product = ProductRepository.GetBy(code);
+
+    return product is not null
+        ? Results.Ok(product)
+        : Results.NotFound();
+}
+
+);
 
 // recebendo do header 
-app.MapGet("/productheader", ( HttpRequest request) => {
+app.MapGet("/productheader", (HttpRequest request) =>
+{
     return request.Headers["product-code"].ToString();
 });
 
-app.MapPut("/product", (Product product ) =>{
+app.MapPut("/product", (Product product) =>
+{
 
     var productSaved = ProductRepository.GetBy(product.Code);
     productSaved.Name = product.Name;
-  
+    return Results.Ok();
+
 });
 
-
-app.MapDelete("/product/{code}", ( [FromRoute] string code)  => {
+app.MapDelete("/product/{code}", ([FromRoute] string code) =>
+{
 
     var productSaved = ProductRepository.GetBy(code);
     ProductRepository.Remove(productSaved);
+    return Results.Ok();
 });
 
 
-app.MapGet("/configuration/database" ,(IConfiguration configuraton) => {
-    return Results.Ok(configuraton["database:port"]);
-});
+if(app.Environment.IsDevelopment())
+    app.MapGet("/configuration/database", (IConfiguration configuraton) =>
+    {
+        return Results.Ok(
+            new
+            {
+                Connection = configuraton["Database:Connection"],
+                Port = configuraton["Database:Port"]
+            }
+                );
+    });
+
 app.Run();
 
 
-public static class ProductRepository{
-    public static List<Product> Products { get; set; }
+public static class ProductRepository
+{
+    public static List<Product>? Products { get; set; } = Products = [];
 
-    public static void Add(Product product){
+    public static void Init(IConfiguration configuration)
+    {
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
+
+
+    public static void Add(Product product)
+    {
         Products ??= [];
-        Products.Add(product);    
+        Products.Add(product);
 
     }
 
-    public static Product GetBy(string code){
+    public static Product GetBy(string code)
+    {
         return Products.FirstOrDefault(p => p.Code == code);
     }
 
-    public static void Remove(Product product){
+    public static void Remove(Product product)
+    {
         Products.Remove(product);
     }
 }
 
 
-public class Product{
+public class Product
+{
     public string? Code { get; set; }
     public string? Name { get; set; }
+}
+
+
+public class ApplicationDBContext : DbContext{
+
 }
--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3885b78e208aa14e4555e522ed8dc156c9f5e00f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f626e66627355d3554576918a15a6880e7ecdea9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
